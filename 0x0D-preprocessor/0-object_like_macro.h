3f6e4ac23c825fa3c0a54b9f8ce3b5a7f3592ede
redctp <redonectp@gmail.com>
2023-07-17 16:16:19 -0700

    update

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H
-#define _OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-/*
- * main - check the code
- *
- * Return: Always 0.
- */
-
-#define size 1024
+#define SIZE 1024
 
 #endif
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,12 +1,6 @@
-#ifndef _PI_H
-#define _PI_H
+#ifndef PI_H
+#define PI_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-#define pi 3.14159265359
+#define PI 3.14159265359
 
 #endif
